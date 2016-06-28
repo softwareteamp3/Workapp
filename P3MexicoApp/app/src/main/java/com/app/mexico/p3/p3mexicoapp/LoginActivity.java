cbcdcd8a60a6d756a4493868a3f3b9c1d89c434a
Charlie Egüez <Charlie Egez>
2016-06-27 20:21:53 -0500

    Agregado Login Activity

---
+++ P3MexicoApp/app/src/main/java/com/app/mexico/p3/p3mexicoapp/LoginActivity.java
@@ -0,0 +1,35 @@
+package com.app.mexico.p3.p3mexicoapp;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Button;
+import android.view.View;
+import android.view.View.OnClickListener;
+
+public class LoginActivity extends AppCompatActivity implements OnClickListener {
+
+    Button bMain;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        bMain = (Button) findViewById(R.id.button);
+
+        bMain.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+
+        switch (v.getId()) {
+            case R.id.button:
+                Intent iMain = new Intent(this, MainActivity.class);
+                startActivity(iMain);
+        }
+
+    }
+}
--- P3MexicoApp/app/src/main/java/com/app/mexico/p3/p3mexicoapp/MainActivity.java
+++ P3MexicoApp/app/src/main/java/com/app/mexico/p3/p3mexicoapp/MainActivity.java
@@ -1,13 +1,44 @@
 package com.app.mexico.p3.p3mexicoapp;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
+import android.support.v4.app.NavUtils;
+import android.support.v4.app.TaskStackBuilder;
+import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends ActionBarActivity {
+
+    private Toolbar toolbar;//Declaramos el Toolbar
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        toolbar = (Toolbar) findViewById(R.id.tool_bar); // Conectamos el Layout al objeto Toolbar
+
+        // Configuración de la barra de herramientas (toolbar ) como la Barra de acciones (ActionBar)
+        // con la llamada de setSupportActionBar ()
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        if (id == R.id.home) {
+            Intent upIntent = NavUtils.getParentActivityIntent(this);
+            if(NavUtils.shouldUpRecreateTask(this,upIntent)){
+                TaskStackBuilder.create(this).addNextIntentWithParentStack(upIntent).startActivities();
+            }else{
+                NavUtils.navigateUpTo(this, upIntent);
+            }
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
+
